c541a38d2b283fb589c9013343f13a58511ab1f9
LeePui <444561897@qq.com>
2018-05-04 10:36:53 +0800

    新建JavaSe项目

---
+++ javaSE_learn/src/main/java/fanxing/LeixingCaChu.java
@@ -0,0 +1,15 @@
+package fanxing;
+
+/**
+ * 类型擦除
+ * Java中的泛型基本上都是在编译器这个层次来实现的。在生成的Java字节码中是不包含泛型中的类型信息的。使用泛型的时候加上的类型参数，会在编译器在编译的时候去掉。这个过程就称为类型擦除。
+ * @author LeePui
+ *
+ */
+public class LeixingCaChu {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
