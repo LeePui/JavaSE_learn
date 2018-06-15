6863d5b85a75ffdd38803bd64af3b787fac7b21d
LeePui <444561897@qq.com>
2018-06-15 11:22:26 +0800

    jvm相关

---
+++ javaSE_learn/src/main/java/jvm/JavaVMStackSOF.java
@@ -0,0 +1,22 @@
+package jvm;
+
+public class JavaVMStackSOF {
+	
+	private int stackLength = 1;
+	
+	public void stackLeak() {
+		stackLength++;
+		stackLeak();
+	}
+	
+	public static void main(String[] args) {
+		JavaVMStackSOF oom = new JavaVMStackSOF();
+		try {
+			oom.stackLeak();
+		} catch (Exception e) {
+			System.out.println("stack length: " + oom.stackLength);
+			throw e;
+		}
+	}
+
+}
